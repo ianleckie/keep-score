d5f305a783746517edc511c07f0a23828da1d813
ianleckie <ianleckie@hey.com>
2021-12-26 22:41:16 -0600

    First working version.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,115 @@
+const KeepScoreElement = document.getElementById("keep-score")
+
+let gamePlayersDefault = [
+		{ name: '', score: 0, added: '', total: 0, turn: false },
+		{ name: '', score: 0, added: '', total: 0, turn: false }
+	]
+
 const KeepScore = {
+	
 	data() {
 		return {
-			gamePlayers: [
-				{ name: '', score: 0 },
-				{ name: '', score: 0 }
-			],
+			gamePlayers: gamePlayersDefault,
+			placeholder: 'Player Name',
 			starting: true,
 			playing: false,
 			ending: false,
 		}
 	},
+	
 	methods: {
+		
 		addPlayer() {
-			this.gamePlayers.push( { name: '', score: 0 } )
+			
+			this.gamePlayers.push( { name: '', score: 0, added: '', total: 0, turn: false } )
+		
 		},
+		
 		startGame() {
+			
 			this.starting = false
 			this.playing = true
+			this.ending = false
+			
+			KeepScoreElement.className = ''
+			KeepScoreElement.classList.add("playing")
+		
+		},
+		
+		playerTurn( event ) {
+
+			if ( this.playing ) {
+
+				var listElement = event.target.closest('li')
+
+				for ( const listItem of document.getElementsByTagName('li') ) {
+					listItem.classList.remove('turn')
+				}
+
+				listElement.classList.add('turn')
+
+				this.addScores()
+
+			}
+
+		},
+
+		addScores() {
+
+			// add scores that need to be added
+			for ( gamePlayer of this.gamePlayers ) {
+				
+				if ( gamePlayer.added != 0 ) {
+					
+					gamePlayer.score = gamePlayer.total
+					gamePlayer.total = 0
+					gamePlayer.added = ''
+				
+				}
+			
+			}
+
+		},
+		
+		endGame( event ) {
+
+			this.starting = false
+			this.playing = false
+			this.ending = true
+
+			this.addScores()
+
+			this.gamePlayers.sort( (a, b) => b.score - a.score );
+
+			KeepScoreElement.className = ''
+			KeepScoreElement.classList.add("ending")
+
+			var listElement = event.target.closest('li')
+
+			for ( const listItem of document.getElementsByTagName('li') ) {
+				listItem.classList.remove('turn')
+			}
+
+		},
+		
+		addScore( event ) {
+
+			let playerId = event.target.nextSibling.value
+
+			this.gamePlayers[playerId].total = Number(this.gamePlayers[playerId].score) + Number(this.gamePlayers[playerId].added)
+
+		},
+
+		showScore() {
+
+			alert('ok')
+
+			return ( this.playing || this.ending ) && !this.starting
+
 		}
+	
 	},
+
 }
 
 Vue.createApp(KeepScore).mount('#keep-score')
